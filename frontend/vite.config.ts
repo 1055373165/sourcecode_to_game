0c538841b048112379f13095a6ae6062f99a4f1a
sunmengyu03 <sunmengyu03@meituan.com>
2026-02-04 20:21:00 +0800

    fix: 添加依赖

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +20,7 @@ export default defineConfig({
     },
     server: {
         port: 5173,
+        host: true,
         proxy: {
             '/api': {
                 target: 'http://localhost:8000',
